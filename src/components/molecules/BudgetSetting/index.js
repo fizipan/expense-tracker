6a465c28ee0d43ef90fb32dc719669674969408d
hafizhmaulanay <56567633+hafizhmaulanay@users.noreply.github.com>
2021-10-25 11:06:22 +0700

    fix budget setting icon

--- src/components/molecules/BudgetSetting/index.js
+++ src/components/molecules/BudgetSetting/index.js
@@ -7,14 +7,30 @@ export default function BudgetSetting() {
 
       {/* Budget Item */}
       <div className="space-y-5 rounded-md mt-4 overflow-y-auto scrollbar-hide max-h-[27.2rem] lg:max-h-[24.6rem]">
-        <BudgetItem name="Wages" value="+ 3.000.000" date="Oct 22, 2021" />
-        <BudgetItem name="Health" value="- 50.000" date="Oct 22, 2021" />
-        <BudgetItem name="Shopping" value="- 150.000" date="Oct 22, 2021" />
-        <BudgetItem name="Dividend" value="+ 200.000" date="Oct 22, 2021" />
-        <BudgetItem name="Wages" value="+ 3.000.000" date="Oct 22, 2021" />
-        <BudgetItem name="Health" value="- 50.000" date="Oct 22, 2021" />
-        <BudgetItem name="Shopping" value="- 150.000" date="Oct 22, 2021" />
-        <BudgetItem name="Dividend" value="+ 200.000" date="Oct 22, 2021" />
+        <BudgetItem
+          name="Wages"
+          action="income"
+          value={3000000}
+          date="Oct 21, 2021"
+        />
+        <BudgetItem
+          name="Health"
+          action="expenditure"
+          value={50000}
+          date="Oct 22, 2021"
+        />
+        <BudgetItem
+          name="Shopping"
+          action="expenditure"
+          value={150000}
+          date="Oct 23, 2021"
+        />
+        <BudgetItem
+          name="Dividend"
+          action="income"
+          value={200000}
+          date="Oct 24, 2021"
+        />
       </div>
     </div>
   );
